2458c6fee448f4f778ad3259aa5026bafabd334a
duzhao <duzhaowork@163.com>
2018-09-18 16:49:23 +0800

    add main.min.js

--- source/js/main.js
+++ source/js/main.js
@@ -16,7 +16,8 @@
         forEach = Array.prototype.forEach,
         even = ('ontouchstart' in w && /Mobile|Android|iOS|iPhone|iPad|iPod|Windows Phone|KFAPWI/i.test(navigator.userAgent)) ? 'touchstart' : 'click',
         isWX = /micromessenger/i.test(navigator.userAgent),
-        noop = function () { },
+        noop = function () {
+        },
         offset = function (el) {
             var x = el.offsetLeft,
                 y = el.offsetTop;
@@ -32,7 +33,7 @@
                 y: y
             };
         },
-        rootScollTop = function() {
+        rootScollTop = function () {
             return d.documentElement.scrollTop || d.body.scrollTop;
         };
 
@@ -104,75 +105,78 @@
                     fixed: noop,
                     actived: noop
                 }
-            }
+            } else {
 
-            var bannerH = $('.post-header').clientHeight,
-                headerH = header.clientHeight,
-                titles = $('#post-content').querySelectorAll('h1, h2, h3, h4, h5, h6');
+                var bannerH = $('.post-header').clientHeight,
+                    headerH = header.clientHeight,
+                    titles = $('#post-content').querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-            var firstLink = titles.length > 0 ? toc.querySelector('a[href="#' + titles[0].id + '"]') : toc.querySelectorAll('a')[0];
 
-            if (firstLink)
-                firstLink.parentNode.classList.add('active');
+                var firstLink = titles.length > 0 ? toc.querySelector('a[href="#' + titles[0].id + '"]') : toc.querySelectorAll('a')[0];
 
-            // Make every child shrink initially
-            var tocChilds = toc.querySelectorAll('.post-toc-child');
-            for (i = 0, len = tocChilds.length; i < len; i++) {
-                tocChilds[i].classList.add('post-toc-shrink');
-            }
-            var firstChild =
-                firstLink.nextElementSibling;
-            if (firstChild) {
-                firstChild.classList.add('post-toc-expand');
-                firstChild.classList.remove('post-toc-shrink');
-            }
-            toc.classList.remove('post-toc-shrink');
+                if (firstLink)
+                    firstLink.parentNode.classList.add('active');
 
-            /**
-             * Handle toc active and expansion
-             * @param prevEle previous active li element
-             * @param currEle current active li element
-             */
-            var handleTocActive = function (prevEle, currEle) {
-                prevEle.classList.remove('active');
-                currEle.classList.add('active');
-
-                var siblingChilds = currEle.parentElement.querySelectorAll('.post-toc-child');
-                for (j = 0, len1 = siblingChilds.length; j < len1; j++) {
-                    siblingChilds[j].classList.remove('post-toc-expand');
-                    siblingChilds[j].classList.add('post-toc-shrink');
+                // Make every child shrink initially
+                var tocChilds = toc.querySelectorAll('.post-toc-child');
+                for (i = 0, len = tocChilds.length; i < len; i++) {
+                    tocChilds[i].classList.add('post-toc-shrink');
                 }
-                var myChild = currEle.querySelector('.post-toc-child');
-                if (myChild) {
-                    myChild.classList.remove('post-toc-shrink');
-                    myChild.classList.add('post-toc-expand');
+                var firstChild =
+                    firstLink.nextElementSibling;
+                if (firstChild) {
+                    firstChild.classList.add('post-toc-expand');
+                    firstChild.classList.remove('post-toc-shrink');
                 }
-            };
+                toc.classList.remove('post-toc-shrink');
 
-            return {
-                fixed: function (top) {
-                    top >= bannerH - headerH ? toc.classList.add('fixed') : toc.classList.remove('fixed');
-                },
-                actived: function (top) {
+                /**
+                 * Handle toc active and expansion
+                 * @param prevEle previous active li element
+                 * @param currEle current active li element
+                 */
+                var handleTocActive = function (prevEle, currEle) {
+                    prevEle.classList.remove('active');
+                    currEle.classList.add('active');
 
-                    if (titles.length == 0) {
-                      titles = $('#post-content').querySelectorAll('h1, h2, h3, h4, h5, h6');
+                    var siblingChilds = currEle.parentElement.querySelectorAll('.post-toc-child');
+                    for (j = 0, len1 = siblingChilds.length; j < len1; j++) {
+                        siblingChilds[j].classList.remove('post-toc-expand');
+                        siblingChilds[j].classList.add('post-toc-shrink');
                     }
+                    var myChild = currEle.querySelector('.post-toc-child');
+                    if (myChild) {
+                        myChild.classList.remove('post-toc-shrink');
+                        myChild.classList.add('post-toc-expand');
+                    }
+                };
 
-                    for (i = 0, len = titles.length; i < len; i++) {
-                        if (top > offset(titles[i]).y - headerH - 5) {
-                            var prevListEle = toc.querySelector('li.active');
-                            var currListEle = toc.querySelector('a[href="#' + titles[i].id + '"]').parentNode;
+                return {
+                    fixed: function (top) {
+                        top >= bannerH - headerH ? toc.classList.add('fixed') : toc.classList.remove('fixed');
+                    },
+                    actived: function (top) {
 
-                            handleTocActive(prevListEle, currListEle);
+                        if (titles.length == 0) {
+                            titles = $('#post-content').querySelectorAll('h1, h2, h3, h4, h5, h6');
                         }
-                    }
 
-                    if (titles.length > 0 && top < offset(titles[0]).y) {
-                        handleTocActive(
-                            toc.querySelector('li.active'),
-                            toc.querySelector('a[href="#' + titles[0].id + '"]').parentNode
-                        );
+
+                        for (i = 0, len = titles.length; i < len; i++) {
+                            if (top > offset(titles[i]).y - headerH - 5) {
+                                var prevListEle = toc.querySelector('li.active');
+                                var currListEle = toc.querySelector('a[href="#' + titles[i].id + '"]').parentNode;
+
+                                handleTocActive(prevListEle, currListEle);
+                            }
+                        }
+
+                        if (titles.length > 0 && top < offset(titles[0]).y) {
+                            handleTocActive(
+                                toc.querySelector('li.active'),
+                                toc.querySelector('a[href="#' + titles[0].id + '"]').parentNode
+                            );
+                        }
                     }
                 }
             }
@@ -468,7 +472,7 @@
 
     var ignoreUnload = false;
     var $mailTarget = $('a[href^="mailto"]');
-    if($mailTarget) {
+    if ($mailTarget) {
         $mailTarget.addEventListener(even, function () {
             ignoreUnload = true;
         });
